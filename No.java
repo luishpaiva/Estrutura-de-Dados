355f0bd24e2c30f8789eb6fa7072120974d492cf
Luis Henrique Paiva <luishp@hotmail.com.br>
2021-11-02 18:35:17 -0300

    Atividade da quarta semana.

---
+++ No.java
@@ -0,0 +1,27 @@
+package atividades;
+
+public class No {
+    private Integer valor;
+    private No proximo;
+
+    public No() {
+        valor = null;
+        proximo = null;
+    }
+
+    public Integer getValor() {
+        return this.valor;
+    }
+    
+    public No getProximo() {
+        return this.proximo;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+}
