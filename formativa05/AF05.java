0ae3c834b0360be651bb3b6b75a35341a4f74b49
Luis Henrique Paiva <luishp@hotmail.com.br>
2021-11-07 10:59:10 -0300

    Atividade da quinta semana.

---
+++ formativa05/AF05.java
@@ -0,0 +1,11 @@
+package atividades.formativa05;
+
+public class AF05 {
+    public static void main(String[] args) {
+        Fibonacci f = new Fibonacci();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Termo " + (i + 1) + ": " + f.fibonacci(i));
+        }
+    }
+}
---
+++ formativa05/Fibonacci.java
@@ -0,0 +1,8 @@
+package atividades.formativa05;
+
+public class Fibonacci {
+    public long fibonacci(int elementos) {
+        if (elementos < 2) return elementos;
+        return fibonacci(elementos - 1) + fibonacci(elementos - 2);
+    }
+}
