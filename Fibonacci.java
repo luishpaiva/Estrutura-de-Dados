b3b0154ea6d0780c9cbf3bb56cc66eddb421f91c
Luis Henrique Paiva <luishp@hotmail.com.br>
2021-11-07 10:52:38 -0300

    Reestruturação do repositório.

--- Fibonacci.java
+++
@@ -1,8 +0,0 @@
-package atividades;
-
-public class Fibonacci {
-    public long fibonacci(int elementos) {
-        if (elementos < 2) return elementos;
-        return fibonacci(elementos - 1) + fibonacci(elementos - 2);
-    }
-}
